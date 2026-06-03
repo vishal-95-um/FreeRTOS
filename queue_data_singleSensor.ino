ed07e2d2a54d75c783a5a098cea4aff7643053ea
vishal 95 um <vishalchhokra9@gmail.com>
2026-06-03 23:52:53 +0530

    Added humidity data so queue is passing structure now

--- queue_data_singleSensor.ino
+++ queue_data_singleSensor.ino
@@ -4,48 +4,64 @@ DHT11 dht11(4);
 
 QueueHandle_t myQueue;
 
-void dhtTask(void *pvParameters) {
+typedef struct {
+  float temp;
+  float humidity;
+} SensorData;
+
+void sensorTask(void *pvParameters) {
+  SensorData data;
 
   while(1) {
-    float temp = getTemp();
-    if(xQueueSend(myQueue, &temp, portMAX_DELAY) == pdTRUE) {
-          Serial.print("Sent: ");
-          Serial.println(temp);
-    }
-    vTaskDelay(pdMS_TO_TICKS(2000));
+
+     data.temp = getTemp();
+     data.humidity = getHumidity();
+
+     xQueueSend(myQueue, &data, portMAX_DELAY); 
+
+     /* Queue Overlflow Prevention techniques
+     1. use 0 instead of portMAX_DELAY
+     2. flush old data when queue is overlflow 
+     if(uxQueueSpacesAvailable(myQueue) == 0) -> xQueueReset(myQueue)
+     3. if(uxQueueSpacesAvailable(myQueue) > 0) -> then do xQueueSend
+     4. xQueueOverwrite(myQueue, &data) -> only when queue size is 1 -> good when intermediate values are useless noise (temperature, battery level)
+     */
+
+     vTaskDelay(pdMS_TO_TICKS(2000));
   }
 }
 
-
 void uartTask(void *pvParameters) {
-  float received_item;
+
+  SensorData receivedData;
 
   while(1) {
-    if(xQueueReceive(myQueue, &received_item, portMAX_DELAY) == pdTRUE) {
-      Serial.print("Received: ");
-      Serial.println(received_item);
+
+    if(xQueueReceive(myQueue, &receivedData, portMAX_DELAY) == pdTRUE) {
+
+      Serial.print("The Temperature is: ");
+      Serial.println(receivedData.temp);
+
+      Serial.print("The Humidity is: ");
+      Serial.println(receivedData.humidity);
     }
+
   }
 
 }
 
 void setup() {
-
  Serial.begin(115200);
 
- myQueue = xQueueCreate(10, sizeof(float));
-
- if(myQueue == NULL) {
-  Serial.println("Queue Creation Failed");
- }
-
- xTaskCreate(dhtTask, "DHT Task", 2048, NULL, 1, NULL);
+ myQueue = xQueueCreate(10, sizeof(SensorData));
+ xTaskCreate(sensorTask, "Sensor Task", 2048, NULL, 1, NULL);
  xTaskCreate(uartTask, "UART Task", 2048, NULL, 1, NULL);
+
 
 }
 
 void loop() {
-
+ 
 
 }
 
@@ -55,6 +71,17 @@ float getTemp()
 
     if (temp != DHT11::ERROR_CHECKSUM && temp != DHT11::ERROR_TIMEOUT) {
         return temp;
+    } else {
+        return -555;
+    }
+}
+
+float getHumidity()
+{
+    float humidity = dht11.readHumidity();
+
+    if (humidity != DHT11::ERROR_CHECKSUM && humidity != DHT11::ERROR_TIMEOUT) {
+        return humidity;
     } else {
         return -555;
     }
